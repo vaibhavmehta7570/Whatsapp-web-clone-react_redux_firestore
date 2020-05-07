50d08c4241e2034b62efffe6c776a8e3185be02e
Sidharth Shankar <sidharth.shankar@mountblue.tech>
2020-05-07 23:32:44 +0530

    done changes in UI and also maps all data flow inside the chat window

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,98 +1,135 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { db } from '../services/firebase';
-import '../assets/styles/Chat.css';
-import Contact from './Contact';
-import user_default from '../assets/images/users.svg';
-import TextBox from './TextBox';
-import search from '../assets/images/search.svg';
-import { getUsers } from '../actions/contactActions';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { db } from "../services/firebase";
+import "../assets/styles/Chat.css";
+import Contact from "./Contact";
+import user_default from "../assets/images/users.svg";
+import TextBox from "./TextBox";
+import search from "../assets/images/search.svg";
+import { getUsers } from "../actions/contactActions";
+import { Link } from "react-router-dom";
+import ChatWindow from "./ChatWindow";
 class Chat extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			searchString: '',
-			searchedUsers: null
-		};
-	}
-	componentDidMount() {
-		db.collection('users')
-			.get()
-			.then(snapshot => {
-				const users = [];
-				snapshot.forEach(doc => {
-					const data = doc.data();
-					users.push(data);
-				});
-				this.props.getUsers(users);
-			})
-			.catch(error => console.log(error));
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: "",
+      searchedUsers: null,
+      currentUserDetail: null,
+      showChatRoom: false,
+    };
+  }
+  componentDidMount() {
+    db.collection("users")
+      .get()
+      .then((snapshot) => {
+        const users = [];
+        snapshot.forEach((doc) => {
+          const data = doc.data();
+          users.push(data);
+        });
+        this.props.getUsers(users);
+      })
+      .catch((error) => console.log(error));
+  }
 
-	// TODO: handle search functionality in search contacts
-	handelOnInputChange = event => {
-		this.setState({ searchString: event.target.value }, () => {
-			this.setState((state, props) => ({
-				searchedUsers: props.users.filter(user =>
-					user.username
-						.toLowerCase()
-						.includes(this.state.searchString.toLowerCase())
-				)
-			}));
-		});
-	};
+  // TODO: handle search functionality in search contacts
+  handelOnInputChange = (event) => {
+    this.setState({ searchString: event.target.value }, () => {
+      this.setState((state, props) => ({
+        searchedUsers: props.users.filter((user) =>
+          user.username
+            .toLowerCase()
+            .includes(this.state.searchString.toLowerCase())
+        ),
+      }));
+    });
+  };
+  openChatRoom = (user) => {
+    this.setState({ currentUserDetail: user, showChatRoom: true });
+  };
 
-	render() {
-		return (
-			<div className='chat-container'>
-				<div className='sidebar'>
-					<div className='user-detail mt-3 ml-4 mb-3'>
-						<img src={user_default} alt='current-user-icon' height='40px' />
-						<Link to='/'>
-							<button
-								type='button'
-								className='logout-button btn btn-primary float-right mr-3'>
-								Logout
-							</button>
-						</Link>
-					</div>
-					<div className='flex mb-2'>
-						<input
-							type='text'
-							placeholder='Search..'
-							className='search'
-							onChange={this.handelOnInputChange}
-						/>
-						<img src={search} alt='search' className='search-icon' />
-					</div>
-					<div className='user_list'>
-						{this.state.searchedUsers
-							? this.state.searchedUsers.map(user => {
-									return <Contact key={user.uid} users={user} />;
-							  })
-							: this.props.users.map(user => {
-									return <Contact key={user.uid} users={user} />;
-							  })}
-					</div>
-				</div>
-				<div className='initial-text tc align-center'>
-					<h1> Welcome!</h1>
-					<h3> Let's connect to the world..</h3>
-				</div>
-				<TextBox />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row p-0">
+          <div className="col-md-4 chat-side-bar p-0">
+            <div className="sidebar">
+              <div className="user-detail mt-3 ml-2 mb-3">
+                <div className="logout-user-dp">
+                  <img
+                    src={user_default}
+                    alt="current-user-icon"
+                    height="40px"
+                  />
+                  <Link to="/">
+                    <button
+                      type="button"
+                      className="logout-button btn btn-primary mr-2"
+                    >
+                      Logout
+                    </button>
+                  </Link>
+                </div>
+              </div>
+              <div className="flex mb-2">
+                <input
+                  type="text"
+                  placeholder="Search.."
+                  className="search"
+                  onChange={this.handelOnInputChange}
+                />
+                <img src={search} alt="search" className="search-icon" />
+              </div>
+              <div className="user_list">
+                {this.state.searchedUsers
+                  ? this.state.searchedUsers.map((user) => {
+                      return (
+                        <Contact
+                          key={user.uid}
+                          users={user}
+                          onClickUser={this.openChatRoom}
+                        />
+                      );
+                    })
+                  : this.props.users.map((user) => {
+                      return (
+                        <Contact
+                          key={user.uid}
+                          users={user}
+                          onClickUser={this.openChatRoom}
+                        />
+                      );
+                    })}
+              </div>
+            </div>
+          </div>
+          {this.state.showChatRoom ? (
+            <ChatWindow userDetails={this.state.currentUserDetail} />
+          ) : (
+            <div className="col-md-8 chat-window before-chat">
+              <div className="welcome-image ml-6 mb-2"></div>
+              <h3 style={{ color: "#525252" }}>Keep your phone connected</h3>
+              <p>
+                WhatsApp connects to your phone to sync messages. To reduce data
+                <br />
+                usage, connect your phone to Wi-Fi.
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
-const mapStateToProps = state => {
-	return {
-		users: state.users
-	};
+const mapStateToProps = (state) => {
+  return {
+    users: state.users,
+  };
 };
-const mapDispatchToProps = dispatch => {
-	return {
-		getUsers: data => dispatch(getUsers(data))
-	};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getUsers: (data) => dispatch(getUsers(data)),
+  };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Chat);
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import user_default from '../assets/images/users.svg';
-import '../assets/styles/Chat.css';
-const Contact = props => {
-	return (
-		<div className='users'>
-			<div className='inline-block pl-3'>
-				<img src={user_default} alt='contact' />
-			</div>
-			<div className='inline-block'>
-				<p className='userName mt-2'>Name: {props.users.username}</p>
-			</div>
-		</div>
-	);
+import React from "react";
+import user_default from "../assets/images/users.svg";
+import "../assets/styles/Chat.css";
+const Contact = (props) => {
+  return (
+    <div className="users" onClick={() => props.onClickUser(props.users)}>
+      <div className="user-dp mt-2 ml-2">
+        <img src={user_default} height="40px" alt="contact" />
+      </div>
+      <div className="users-name">
+        <p className="userName mt-3">Name: {props.users.username}</p>
+      </div>
+    </div>
+  );
 };
 export default Contact;
