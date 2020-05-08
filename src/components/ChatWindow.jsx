04b0b3761fdf7da94b1c2c43f5561c872c93620b
Sidharth Shankar <sidharth.shankar@mountblue.tech>
2020-05-08 15:31:34 +0530

    debuged that every user is now now receiving it's own messages

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -15,10 +15,9 @@ class ChatWindow extends Component {
       email: this.props.currentUser.email,
     };
   }
-  // componentDidMount() {
-  //   console.log('mounted')
-  //   this.props.fetchMessages(this.props.message, this.props.newChatDocRef);
-  // }
+  componentDidMount() {
+    this.props.fetchMessages(this.props.message, this.props.newChatDocRef);
+  }
   handleOnchange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
@@ -65,6 +64,7 @@ class ChatWindow extends Component {
           </div>
           <div className="message-area">
             {this.props.message.map((message) =>
+              message !== undefined &&
               message.sender_id !== this.state.email ? (
                 <ReceiverCard
                   key={message.message_id}
